337293dce9f46a8d9fbd7a747505abbc33988f2c
Adam Tovatt <sporedenstore@hotmail.com>
2023-04-20 21:36:26 +0200

    setup camera server

---
+++ CameraServer/Controllers/CameraController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Sakur.WebApiUtilities.Models;
+using System.Net;
+
+namespace CameraServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CameraController : ControllerBase
+    {
+        [HttpGet("hello")]
+        public async Task<ObjectResult> Hello()
+        {
+            try
+            {
+                await Task.Delay(1);
+                return new ApiResponse("Hello World!", HttpStatusCode.OK);
+            }
+            catch (ApiException exception)
+            {
+                return new ApiResponse(exception);
+            }
+        }
+    }
+}
