cdca3f0eaf3510dd4d89962fe2a03eba6801f098
Naif Hagatulah <90781179+NaifHagatulah@users.noreply.github.com>
2023-04-22 18:14:49 +0200

    Camera controller tests (#10)
    * add method getImage in cameraController class
    
    * fixed requested fixes from review
    
    * fixed pull request issues
    
    * add CameraControllerTests.cs
    
    * reset CameraController to master
    
    * resolved some issues with the reset to master
    
    * add test for controller hello method
    
    * add test for image method in CameraController
    
    * add CustomAsserts.cs
    
    * add improvment to CameraControllerTest hello method all tests pass now
    
    ---------
    
    Co-authored-by: Adam Tovatt <sporedenstore@hotmail.com>

---
+++ CameraServerTests/Tests/CameraControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using CameraServer.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CameraServerTests.Tests
+{
+    [TestClass]
+    public class CameraControllerTests
+    {
+        [TestMethod]
+        public async Task Hello()
+        {
+            // Arrange
+            CameraController controller = new CameraController();
+
+            // Act
+            ObjectResult result = await controller.Hello();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Value);
+            Assert.That.ObjectsHaveSameProperties(new { message = "Hello World!" }, result.Value);
+        }
+
+        [TestMethod]
+        public async Task GetImage()
+        {
+            // Arrange
+            CameraController controller = new CameraController();
+
+            // Act
+            var result = await controller.GetImage();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("image/jpeg", result.ContentType);
+            Assert.AreEqual(239172, result.FileContents.Length); // check that the amount of bytes that were read is correct
+        }
+    }
+}
