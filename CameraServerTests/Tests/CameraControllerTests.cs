a0eac4b550e056d14225b442babbc67402acdb68
Naif <naif@kth.se>
2023-04-22 17:44:17 +0200

    add test for controller hello method

--- CameraServerTests/Tests/CameraControllerTests.cs
+++ CameraServerTests/Tests/CameraControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CameraServer.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace CameraServerTests.Tests
 {
+    [TestClass]
     internal class CameraControllerTests
     {
+        [TestMethod]
+        public async Task Hello()
+        {
+            // Arrange
+            CameraController controller = new CameraController();
+
+            // Act
+            var result = await controller.Hello();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Hello World!", result);
+        }
     }
 }
