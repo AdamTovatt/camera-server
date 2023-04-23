33cc8a7b2beba9d198fcf0f0ed2ec92fc4a8ae42
Adam Tovatt <sporedenstore@hotmail.com>
2023-04-23 16:34:32 +0200

    clean up code since we use opencv now

--- CameraServer/Models/ImageCapturing/LocalCameraImageProvider.cs
+++ CameraServer/Models/ImageCapturing/LocalCameraImageProvider.cs
@@ -1,40 +1,21 @@
-﻿using MMALSharp.Common;
-using MMALSharp.Handlers;
-using MMALSharp;
-using OpenCvSharp;
+﻿using OpenCvSharp;
 
 namespace CameraServer.Models.ImageCapturing
 {
     public class LocalCameraImageProvider : IImageProvider
     {
         private static FrameSource? frameSource = null;
+        private static Mat? mat = null;
 
         public async Task<byte[]> GetImageBytes()
         {
             if (frameSource == null)
                 frameSource = Cv2.CreateFrameSource_Camera(0);
+            if(mat == null)
+                mat = new Mat();
 
-            Mat mat = new Mat();
-            frameSource.NextFrame(mat);
-
-            await Task.CompletedTask;
-
+            await Task.Run(() => { frameSource.NextFrame(mat); });
             return mat.ToBytes();
-
-            /*
-            // Singleton initialized lazily. Reference once in your application.
-            MMALCamera cam = MMALCamera.Instance;
-
-            using (var imgCaptureHandler = new ByteArrayCaptureHandler())
-            {
-                await cam.TakePicture(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420);
-                cam.Cleanup();
-
-                if (imgCaptureHandler.WorkingData == null)
-                    throw new Exception("Working data is null");
-                
-                return imgCaptureHandler.WorkingData;
-            }*/
         }
     }
 }
