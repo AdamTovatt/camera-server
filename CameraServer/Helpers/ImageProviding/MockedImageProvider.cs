26d78854842b10b00d67feb01a30b54418162edc
Adam Tovatt <sporedenstore@hotmail.com>
2023-04-28 18:19:01 +0200

    add CameraImage to use instead of a byte[]

---
+++ CameraServer/Helpers/ImageProviding/MockedImageProvider.cs
@@ -0,0 +1,12 @@
+﻿using CameraServer.Models;
+
+namespace CameraServer.Helpers.ImageProviding
+{
+    public class MockedImageProvider : IImageProvider
+    {
+        public async Task<CameraImage> GetImage()
+        {
+            return new CameraImage(await EmbeddedResourceHelper.ReadResource("MockedCameraImage"));
+        }
+    }
+}
