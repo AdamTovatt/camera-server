db996ab5a779656de63071a5f947af77f9179069
Adam Tovatt <sporedenstore@hotmail.com>
2023-05-01 23:02:03 +0200

    don't use https redirection

--- CameraServer/Program.cs
+++ CameraServer/Program.cs
@@ -22,7 +22,7 @@ namespace CameraServer
                 app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
