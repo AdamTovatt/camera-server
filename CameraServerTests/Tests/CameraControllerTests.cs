d98dce729350fb04996dbceda20f73bf10173bcb
Naif <naif@kth.se>
2023-05-01 19:58:59 +0200

    add all tests for controller

--- CameraServerTests/Tests/CameraControllerTests.cs
+++ CameraServerTests/Tests/CameraControllerTests.cs
@@ -46,32 +46,64 @@ namespace CameraServerTests.Tests
         }
 
         [TestMethod]
-        public async Task AssertUpdateAndGetCameraImageCorrect()
+        public async Task AssertUpdateCameraImageCorrect()
         {
             // Arrange
             CameraController controller = new CameraController();
             using MemoryStream? stream = await TestUtilities.GetTestFileAsync("MockedCameraImage");
+            {
+                if (stream == null)
+                    Assert.Fail("Could not load MockedCameraImage");
 
-            if (stream == null)
-                Assert.Fail("Could not load MockedCameraImage");
-
-            IFormFile image = TestUtilities.GetIFormFile(stream);
+                IFormFile image = TestUtilities.GetIFormFile(stream);
+                await controller.UpdateCameraImage(image, 1);
+                await controller.UpdateCameraImage(image, 2);
+            }
 
             // Act
-            await controller.UpdateCameraImage(image, 1);
             FileContentResult imageFromController = await controller.GetCameraImage(1);
 
             // Assert
             Assert.AreEqual(239172, imageFromController.FileContents.Length);
             Assert.AreEqual("image/jpeg", imageFromController.ContentType);
+            Assert.AreEqual(1, CameraContainer.Instance.CameraCount);
+            Assert.AreEqual(239172, (await (await CameraContainer.Instance.GetCameraAsync(1)).GetImageAsync()).Bytes.Length);
 
             // Act2
-            await controller.UpdateCameraImage(image, 2);
             FileContentResult imageFromController2 = await controller.GetCameraImage(2);
 
             // Assert
             Assert.AreEqual(239172, imageFromController2.FileContents.Length);
             Assert.AreEqual("image/jpeg", imageFromController2.ContentType);
+            Assert.AreEqual(2, CameraContainer.Instance.CameraCount);
+            Assert.AreEqual(239172, (await (await CameraContainer.Instance.GetCameraAsync(2)).GetImageAsync()).Bytes.Length);
+        }
+
+        public async Task AssertGetCameraImageCorrect()
+        {
+            // Arrange
+            CameraController controller = new CameraController();
+            using MemoryStream? stream = await TestUtilities.GetTestFileAsync("MockedCameraImage");
+            {
+                if (stream == null)
+                    Assert.Fail("Could not load MockedCameraImage");
+
+                IFormFile image = TestUtilities.GetIFormFile(stream);
+                ObjectResult result1 = await controller.UpdateCameraImage(image, 1);
+                ObjectResult result2 = await controller.UpdateCameraImage(image, 2);
+
+                // Assert
+                Assert.IsNotNull(result1);
+                Assert.IsNotNull(result2);
+                Assert.AreEqual(200, result1.StatusCode);
+                Assert.AreEqual(200, result2.StatusCode);
+            }
+
+            // Assert
+            Assert.AreEqual(1, CameraContainer.Instance.CameraCount);
+            Assert.AreEqual(239172, (await (await CameraContainer.Instance.GetCameraAsync(1)).GetImageAsync()).Bytes.Length);
+            Assert.AreEqual(2, CameraContainer.Instance.CameraCount);
+            Assert.AreEqual(239172, (await (await CameraContainer.Instance.GetCameraAsync(2)).GetImageAsync()).Bytes.Length);
         }
     }
 }
