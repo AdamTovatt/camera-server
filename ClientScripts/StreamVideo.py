d952f0564f8e3f6699a840b357d58cb0300f39b1
Adam Tovatt <sporedenstore@hotmail.com>
2023-05-06 18:28:33 +0200

    improve information logging in StreamVideo script, add reading from config

--- ClientScripts/StreamVideo.py
+++ ClientScripts/StreamVideo.py
@@ -2,21 +2,65 @@ import cv2
 import websocket
 import struct
 import time
+import json
+import os
+
+
+class CameraConfig:
+    def __init__(self, cameraId, cameraToken, webSocketEndpoint):
+        self.cameraId = cameraId
+        self.cameraToken = cameraToken
+        self.webSocketEndpoint = webSocketEndpoint
+
+    @staticmethod
+    def readConfigFromFile(path):
+        with open(path, 'r') as file:
+            config_data = json.load(file)
+
+        camera_id = config_data['cameraId']
+        camera_token = config_data['cameraToken']
+        backend_url = config_data['webSocketEndpoint']
+
+        return CameraConfig(camera_id, camera_token, backend_url)
+
+
+configPath = "camera-config.json"
 
 running = True
 cap = None
 ws = None
+config = None
+
+if not os.path.exists(configPath):
+    print("Error! No config file found.")
+    print("Should be a config file called camera-config.json in the same directory as this script.")
+    print("The config file should contain: cameraId, cameraToken, webSocketEndpoint.")
+    running = False
+else:
+    try:
+        config = CameraConfig.readConfigFromFile(configPath)
+    except json.decoder.JSONDecodeError:
+        print("Invalid json config file, please check the file and try again.")
+        print("The config file should contain: cameraId, cameraToken, webSocketEndpoint.")
+        running = False
 
 while running:
     try:
+        print("Connecting to the server at " + config.webSocketEndpoint)
         # Connect to the server using WebSocket (Important note! The port is 5000, not 5001! It doesn't work with 5001, I don't know why)
-        ws = websocket.create_connection(
-            "ws://localhost:5000/video-input-stream")
+        ws = websocket.create_connection(config.webSocketEndpoint)
+
+        print("Did connect to the server, sending camera ID")
+
         # Send the camera ID to the server
         ws.send_binary(struct.pack('<I', 1337))
 
+        print("Did send camera ID, starting video capture")
+
         # Open the camera
         cap = cv2.VideoCapture(0)
+
+        print("Video capture started, will start sending video")
 
         # Capture and encode the video
         while running:
@@ -35,13 +79,14 @@ while running:
             time.sleep(0.01)
     except ConnectionResetError:
         print(
-            "The established connection to the server was lost, will attempt to reconnect.")
+            "The established connection to the server was lost, will attempt to reconnect")
         cap.release()  # Release the camera resource since we don't know if we will be able to reconnect
     except ConnectionRefusedError:
-        print("Could not establish a new connection to the server, retrying after 5 seconds...")
+        print(
+            "Could not establish a new connection to the server, retrying after 5 seconds")
         time.sleep(5)
     except ConnectionAbortedError:
-        print("The server closed the connection, will attempt to reconnect in 5 seconds...")
+        print("The server closed the connection, will attempt to reconnect in 5 seconds")
         time.sleep(5)
 
         # Release the resources
